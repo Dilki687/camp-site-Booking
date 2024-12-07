7a86fe3970430139eb704443abab746b9f3f04dd
Dilki687 <chamodidilki44@gmail.com>
2024-12-07 21:31:59 +0530

    for backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,14 @@ const camplocationsRoute = require('./routes/camplocationsRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
 const reservationRoutes = require('./routes/reservationRoutes');
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://camp-site-booking-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+
 app.use(express.json())
 
 app.use('/api/camplocations' , camplocationsRoute)
