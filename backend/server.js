1344f6f83c8ef2bfc297587af4e57b55dd0143e7
Dilki687 <chamodidilki44@gmail.com>
2024-12-07 23:55:24 +0530

    changes -  package.json

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ const reservationRoutes = require('./routes/reservationRoutes');
 
 app.use(cors(
     {
-        origin: ["https://camp-site-booking-frontend.vercel.app"],
+        origin: ["https://camp-site-booking-frontend.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
 ));
 
+// app.use(cors());
 app.use(express.json())
 
 app.use('/api/camplocations' , camplocationsRoute)
